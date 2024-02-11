dc5b3da7a0eda6d191d12153a4cbdcb74756f8d5
crazy <fuyoumingyan@gmail.com>
2024-02-11 13:42:25 +0800

    进度条

--- progress/progressbar.go
+++ progress/progressbar.go
@@ -7,25 +7,47 @@ import (
 	"time"
 )
 
-func NewProgressbar(max int64, description string) *progressbar.ProgressBar {
-	return progressbar.NewOptions64(max,
-		progressbar.OptionSetDescription(description),
-		progressbar.OptionSetWriter(ansi.NewAnsiStdout()),
-		progressbar.OptionOnCompletion(func() {
-			fmt.Fprint(ansi.NewAnsiStdout(), "\n")
-		}),
-		progressbar.OptionSetRenderBlankState(true),
-		progressbar.OptionEnableColorCodes(true),
-		progressbar.OptionSetWidth(15),
-		progressbar.OptionShowIts(),
-		progressbar.OptionShowCount(),
-		progressbar.OptionFullWidth(),
-		progressbar.OptionThrottle(65*time.Millisecond),
-		progressbar.OptionSetTheme(progressbar.Theme{
-			Saucer:        "[green]=[reset]",
-			SaucerHead:    "[green]>[reset]",
-			SaucerPadding: " ",
-			BarStart:      "[",
-			BarEnd:        "]",
-		}))
+func NewProgressbar(max int64, description string, show bool) *progressbar.ProgressBar {
+	// show 表示当进度条走完后是否在终端上面显示 还是就消失了
+	if show {
+		return progressbar.NewOptions64(max,
+			progressbar.OptionSetDescription(description),
+			progressbar.OptionSetWriter(ansi.NewAnsiStdout()),
+			progressbar.OptionOnCompletion(func() {
+				fmt.Fprint(ansi.NewAnsiStdout(), "\n")
+			}),
+			progressbar.OptionSetRenderBlankState(true),
+			progressbar.OptionEnableColorCodes(true),
+			progressbar.OptionSetWidth(15),
+			progressbar.OptionShowIts(),
+			progressbar.OptionShowCount(),
+			progressbar.OptionFullWidth(),
+			progressbar.OptionThrottle(65*time.Millisecond),
+			progressbar.OptionSetTheme(progressbar.Theme{
+				Saucer:        "[green]=[reset]",
+				SaucerHead:    "[green]>[reset]",
+				SaucerPadding: " ",
+				BarStart:      "[",
+				BarEnd:        "]",
+			}))
+	} else {
+		return progressbar.NewOptions64(max,
+			progressbar.OptionSetDescription(description),
+			progressbar.OptionSetWriter(ansi.NewAnsiStdout()),
+			progressbar.OptionSetRenderBlankState(true),
+			progressbar.OptionEnableColorCodes(true),
+			progressbar.OptionSetWidth(15),
+			progressbar.OptionShowIts(),
+			progressbar.OptionShowCount(),
+			progressbar.OptionFullWidth(),
+			progressbar.OptionThrottle(65*time.Millisecond),
+			progressbar.OptionSetTheme(progressbar.Theme{
+				Saucer:        "[green]=[reset]",
+				SaucerHead:    "[green]>[reset]",
+				SaucerPadding: " ",
+				BarStart:      "[",
+				BarEnd:        "]",
+			}))
+	}
+
 }
